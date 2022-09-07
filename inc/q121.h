ead7a72a29a58688fe466acd02f3424a4e9af4fd
AmamiyaRenn <379617892@qq.com>
2022-09-07 20:35:39 +0800

    q121

--- inc/q121.h
+++ inc/q121.h
@@ -1,1 +1,17 @@
 #include "including.h"
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int ans = 0, priceMin = prices[0];
+        for (int price : prices)
+        {
+            int profit = price - priceMin;
+            ans = profit > ans ? profit : ans;
+            priceMin = priceMin > price ? price : priceMin;
+        }
+        return ans;
+    }
+};
