49aa0b2df156fd09c5c315bb264e744ccb0990f1
AmamiyaRenn <379617892@qq.com>
2022-09-07 20:20:20 +0800

    完成了q350

---
+++ inc/q121.h
@@ -0,0 +1,1 @@
+#include "including.h"
--- inc/q350.h
+++ inc/q350.h
@@ -5,5 +5,18 @@ class Solution
 public:
     vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
     {
+        vector<int> result;
+        for (int i = 0; i < nums1.size(); i++)
+            for (int j = 0; j < nums2.size(); j++)
+                if (nums1[i] == nums2[j])
+                {
+                    result.push_back(nums1[i]);
+                    nums1.erase(nums1.begin() + i);
+                    i--;
+                    nums2.erase(nums2.begin() + j);
+                    j--;
+                    break;
+                }
+        return result;
     }
 };
