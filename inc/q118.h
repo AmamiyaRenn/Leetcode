f18c5281d9818b1e8346db05d91c98889f8ce406
AmamiyaRenn <379617892@qq.com>
2022-09-08 17:37:00 +0800

    完成了数据结构入门day4

---
+++ inc/q118.h
@@ -0,0 +1,26 @@
+﻿#include "including.h"
+
+class Solution
+{
+public:
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> ans;
+        ans.push_back(vector<int>{1});
+        if (numRows == 1)
+            return ans;
+        ans.push_back(vector<int>{1, 1});
+        if (numRows == 2)
+            return ans;
+
+        for (int i = 1; i < numRows - 1; i++)
+        {
+            vector<int> temp{1};
+            for (int j = 0; j < i; j++)
+                temp.push_back(ans[i][j] + ans[i][j + 1]);
+            temp.push_back(1);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
---
+++ inc/q566.h
@@ -0,0 +1,30 @@
+﻿#include "including.h"
+
+class Solution
+{
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>> &mat, int r, int c)
+    {
+        int r_mat = mat.size(), c_mat = mat[0].size();
+        if (r_mat * c_mat != r * c)
+            return mat;
+        vector<vector<int>> result;
+        int p_r = 0, p_c = 0;
+        for (int i = 0; i < r; i++)
+        {
+            vector<int> temp;
+            for (int j = 0; j < c; j++)
+            {
+                temp.push_back(mat[p_r][p_c]);
+                p_c++;
+                if (p_c == c_mat)
+                {
+                    p_c = 0;
+                    p_r++;
+                }
+            }
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
