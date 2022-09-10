fdcad63073e58e36219e9a7eaee11847b3f01585
AmamiyaRenn <379617892@qq.com>
2022-09-10 12:30:51 +0800

    完成了数据结构入门day5

---
+++ inc/q36.h
@@ -0,0 +1,51 @@
+﻿#include "including.h"
+
+class Solution
+{
+public:
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+                board[i][j] = board[i][j] == '.' ? 0 : board[i][j] - '0';
+        // row
+        for (int i = 0; i < 9; i++)
+        {
+            bool *nums = new bool[10]();
+            for (int j = 0; j < 9; j++)
+            {
+                if (nums[board[i][j]] == true && board[i][j] != 0)
+                    return false;
+                nums[board[i][j]] = true;
+            }
+            delete[] nums;
+        }
+        // col
+        for (int j = 0; j < 9; j++)
+        {
+            bool *nums = new bool[10]();
+            for (int i = 0; i < 9; i++)
+            {
+                if (nums[board[i][j]] == true && board[i][j] != 0)
+                    return false;
+                nums[board[i][j]] = true;
+            }
+            delete[] nums;
+        }
+        // grid
+        for (int i1 = 0; i1 < 9; i1 += 3)
+            for (int i2 = 0; i2 < 9; i2 += 3)
+            {
+                bool *nums = new bool[10]();
+                for (int j1 = 0; j1 < 3; j1++)
+                    for (int j2 = 0; j2 < 3; j2++)
+                    {
+                        if (nums[board[i1 + j1][i2 + j2]] == true && board[i1 + j1][i2 + j2] != 0)
+                            return false;
+                        nums[board[i1 + j1][i2 + j2]] = true;
+                    }
+                delete[] nums;
+            }
+        return true;
+    }
+};
---
+++ inc/q73.h
@@ -0,0 +1,23 @@
+﻿#include "including.h"
+
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int row = matrix.size(), col = matrix[0].size();
+        bool *a_row = new bool[row](), *a_col = new bool[col]();
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                if (matrix[i][j] == 0)
+                    a_row[i] = a_col[j] = true;
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+            {
+                if (a_row[i])
+                    matrix[i][j] = 0;
+                if (a_col[j])
+                    matrix[i][j] = 0;
+            }
+    }
+};
